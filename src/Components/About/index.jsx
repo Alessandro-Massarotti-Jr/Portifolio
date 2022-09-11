f64475f746a872be711043649e98afcffb7738a8
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-09-11 19:50:32 -0300

    change about text

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -6,7 +6,7 @@ export default function About() {
             <h2 className={styles.sectionTitle}>About</h2>
             <div className={styles.aboutContent}>
                 <div className={styles.aboutText}>
-                    <p>Olá, sou Alessandro Massarotti Junior, Desenvolvedor Web junior, sou formado em Analise e desenvolvimento de sistemas e atuo na area de desenvolvimento a cerca de 1 ano.</p>
+                    <p>Olá, sou Alessandro Massarotti Junior, Desenvolvedor Web junior, sou formado em Analise e desenvolvimento de sistemas e atuo na area de desenvolvimento desde setembro de 2021.</p>
                     <p>Atualmente trabalho com o desenvolvimento de APIs com o framework laravel, porém já obtive experiencias com o desenvolvimento de interfaces com a biblioteca React e de temas completos para o CMS WordPress.</p>
                     <p>Estou estudando Node.js e TypeScrypt para poder ser independente em projetos completos com javascript, atuando tanto no FrontEnd quanto no BackEnd.</p>
                 </div>
