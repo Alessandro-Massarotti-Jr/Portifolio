d1b81a1d377afdd1b829b4d35b2a6d304c36453d
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2022-10-22 13:34:14 -0300

    responsive projects

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -2,18 +2,43 @@ import ProjectCard from "../ProjectCard";
 import style from "./styles.module.css"
 
 import { projects } from "../../config/projects";
+import AliceCarousel from "react-alice-carousel";
 
-export default function Projects(){
+export default function Projects() {
 
-    return(
+    return (
         <section className={style.project} id="projects">
             <h2 className={style.sectionTitle}>Projetos</h2>
-             <div className={style.projectCardContainer}>
+            <div className={style.projectCardContainer}>
 
-                {projects.map((project)=>{
-                    return <ProjectCard title={project.title} description={project.description} mainColor={project.mainColor} image={project.image} demo={project.demo} repo={project.repo} tecnologyIcon={project.tecnologyIcon}/>
+                {projects.map((project) => {
+                    return <ProjectCard title={project.title} description={project.description} mainColor={project.mainColor} image={project.image} demo={project.demo} repo={project.repo} tecnologyIcon={project.tecnologyIcon} />
                 })}
-             </div>
+            </div>
+            <div className={style.mobile__projectCardContainer}>
+                <AliceCarousel
+                    autoPlay={true}
+                    autoPlayInterval={2000}
+                    infinite={true}
+                    disableDotsControls={true}
+                    disableButtonsControls={true}
+                    mouseTracking={true}
+                    items={projects.map((project) => {
+                        return <ProjectCard title={project.title} description={project.description} mainColor={project.mainColor} image={project.image} demo={project.demo} repo={project.repo} tecnologyIcon={project.tecnologyIcon} />
+                    })}
+                    responsive={
+                        {
+                            0: {
+                                items: 1,
+                            },
+                            1024: {
+                                items: 5
+                            }
+                        }
+                    }
+                />
+            </div>
+
         </section>
     );
 }
