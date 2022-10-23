e81ad430724020f86e35e837f2ad3e3f5a09f9ff
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2022-10-23 20:40:39 -0300

    change menu order

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -19,10 +19,10 @@ export default function Header() {
             <nav className={styles.desktopNav}>
                 <ul className={styles.navLinks}>
                     <li className={styles.navLink}>
-                        <a href="#about">Sobre</a>
+                        <a href="#tecnologies">Tecnologias</a>
                     </li>
                     <li className={styles.navLink}>
-                        <a href="#tecnologies">Tecnologias</a>
+                        <a href="#about">Sobre</a>
                     </li>
                     <li className={styles.navIcon}>
                         <a href="#welcome">
@@ -55,10 +55,10 @@ export default function Header() {
                     <nav>
                         <ul className={styles.navLinks}>
                             <li className={styles.navLink}>
-                                <a href="#about">Sobre</a>
+                                <a href="#tecnologies">Tecnologias</a>
                             </li>
                             <li className={styles.navLink}>
-                                <a href="#tecnologies">Tecnologias</a>
+                                <a href="#about">Sobre</a>
                             </li>
                             <li className={styles.navLink}>
                                 <a href="#projects">Projetos</a>
