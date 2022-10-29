eaa360c28944c364cd159a5e09b56841ebb80ade
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2022-10-29 18:36:53 -0300

    add contact info

--- src/Components/ContactForm/index.jsx
+++ src/Components/ContactForm/index.jsx
@@ -1,5 +1,6 @@
 import { Icon } from "@iconify/react";
 import { useRef } from "react";
+import ContactInfo from "../ContactInfo";
 import InputText from "../Form/InputText";
 import TextArea from "../Form/TextArea";
 import styles from "./styles.module.css";
@@ -41,17 +42,21 @@ export default function ContactForm() {
     return (
         <section className={styles.contact} id="contact">
             <h2 className={styles.sectionTitle}>Contato</h2>
-            <form className={styles.contactForm} onSubmit={(event) => { handleFormSubmit(event) }}>
-                <InputText name="name" label="Seu Nome (*)" placeholder="digite seu nome..." ref={nameRef} required={true} type="text" />
-                <InputText name="email" label="Seu Email (*)" placeholder="digite seu email..." ref={emailRef} required={true} type="text" />
-                <InputText name="phone" label="Seu telefone" placeholder="digite seu telefone..." ref={phoneRef} required={false} type="text" />
-                <InputText name="subject" label="Assunto (*)" placeholder="digite o assunto..." ref={subjectRef} required={true} type="text" />
-                <TextArea name="message" label="Mensagem (*)" placeholder="digite sua mensagem..." ref={messageRef} required={true} />
-                <button className={styles.form__buttonSubmit} type="submit">
-                    Enviar
-                    <Icon className={styles.button__icon} icon="bi:send" />
-                </button>
-            </form>
+            <div className={styles.contact__content}>
+                <form className={styles.contactForm} onSubmit={(event) => { handleFormSubmit(event) }}>
+                    <InputText name="name" label="Seu Nome (*)" placeholder="digite seu nome..." ref={nameRef} required={true} type="text" />
+                    <InputText name="email" label="Seu Email (*)" placeholder="digite seu email..." ref={emailRef} required={true} type="mail" />
+                    <InputText name="phone" label="Seu telefone" placeholder="digite seu telefone..." ref={phoneRef} required={false} type="text" />
+                    <InputText name="subject" label="Assunto (*)" placeholder="digite o assunto..." ref={subjectRef} required={true} type="text" />
+                    <TextArea name="message" label="Mensagem (*)" placeholder="digite sua mensagem..." ref={messageRef} required={true} />
+                    <button className={styles.form__buttonSubmit} type="submit">
+                        Enviar
+                        <Icon className={styles.button__icon} icon="bi:send" />
+                    </button>
+                </form>
+                <ContactInfo />
+            </div>
+
         </section>
 
     );
