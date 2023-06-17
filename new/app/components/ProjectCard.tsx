66d321aed44abde4fa5fbd4e67db758a23a129b1
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2023-06-17 00:07:08 -0300

    add photo

--- new/app/components/ProjectCard.tsx
+++ new/app/components/ProjectCard.tsx
@@ -7,16 +7,11 @@ import teste from "../assets/img/mockups/teste.png";
 export default function ProjectCard() {
 
     return (
-        <div className='  w-96 flex flex-col items-center'>
-
-
-
-
+        <div className='p-3 text-center w-96 flex flex-col items-center bg-gradient-to-b from-gray-900 to-gray-600 bg-gradient-to-l rounded-md'>
             <Image className="relative z-50" src={teste} alt="celphone" width={666} height={375} />
-            <h3 className="text-3xl">Nome do projeto</h3>
-
-            <p className=" max-w-full text-center"> Este é um projeto legal e bacana, lorem ipsom dolor sit asimet  Este é um projeto legal e bacana, lorem ipsom dolor sit asimet  Este é um projeto legal e bacana, lorem ipsom dolor sit asimet</p>
-
+            <a className="text-center text-3xl font-bold" href="https://adamantiun.next.dev.br/" target="_blank">Market Master</a>
+            <a className="text-center text-blue-300 font-bold" href="https://next.tec.br/" target="_blank">Next Tecnologia</a>
+            <p className=" max-w-full text-center">Sistema de gestão para vendedores do mercado livre.Realiza calculos de lucro contabilização de vendas e controle de estoque consultando a Api do ML.</p>
         </div>
     )
 
