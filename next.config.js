6258fa85f80e3306e7172c4824d81652ccd7a846
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2024-12-24 00:18:35 -0300

    feat(docker-compose): adicionando docker-compose para desenvolvimento

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 500,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
